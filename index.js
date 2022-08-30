a9c9fca2fff3d9580aeceecb75fa6889cc728400
Chloe Scheffler <chloescheffler@gmail.com>
2022-08-29 21:15:12 -0400

    commit tree c5b8e8fad9117432a5585329472726a2bc0a212f

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 2; 
+let num2 = 31;
+let multiply = num1 * num2
 
-// Write your code here
+let random = Math.floor(Math.random() * 1000) + 1;
+
+let num3 = 22;
+let num4 = 18;
+let mod = num3 % num4;
+
+let max = Math.max(2, 4, 6 , 8, 20);
